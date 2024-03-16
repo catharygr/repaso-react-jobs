f9c0b130b04a0a128318c180e83ff254090306c9
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-16 16:32:29 +0000

    respaso para entrevistas

--- repaso-entrevista/src/App.jsx
+++ repaso-entrevista/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Example from "./componentes/Example";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <Example />
+    </div>
+  );
 }
 
 export default App;

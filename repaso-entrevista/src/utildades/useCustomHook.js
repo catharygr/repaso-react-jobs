9cc1669e45a67fb80fa04cd0681ebd0e572b310f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-01 17:18:24 +0100

    Trabajando en el custon hook useCustomHook

--- repaso-entrevista/src/utildades/useCustomHook.js
+++ repaso-entrevista/src/utildades/useCustomHook.js
@@ -4,7 +4,11 @@ export default function useCustomHook() {
   const [value, setValue] = useState(0);
 
   const incrementValue = () => setValue(value + 1);
-  const decrementValue = () => setValue(value - 1);
+  const decrementValue = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  };
 
   return { value, incrementValue, decrementValue };
 }

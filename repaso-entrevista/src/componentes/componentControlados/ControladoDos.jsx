b7acb2ff677c146aef9723c19ca2a2b9bf5a0d0c
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-02 16:14:51 +0100

    Trabajando con ControladoDos.jsx

--- repaso-entrevista/src/componentes/componentControlados/ControladoDos.jsx
+++ repaso-entrevista/src/componentes/componentControlados/ControladoDos.jsx
@@ -6,29 +6,40 @@ export default function ControladoDos({
   handleSubmit,
 }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form
+      className="form"
+      onSubmit={handleSubmit}
+    >
+      <label className="label">
         Nombre
         <input
+          className="input"
           type="text"
           ref={nombreRef}
         />
       </label>
-      <label>
+      <label className="label">
         Email
         <input
+          className="input"
           type="email"
           ref={emailRef}
         />
       </label>
-      <label>
+      <label className="label">
         Password
         <input
+          className="input"
           type="password"
           ref={passwordRef}
         />
       </label>
-      <button type="submit">Enviar</button>
+      <button
+        className="btn"
+        type="submit"
+      >
+        Enviar
+      </button>
     </form>
   );
 }

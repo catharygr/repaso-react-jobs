9a34f4298b22cb6f95a6083bff02d6c8731a86a4
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-02 15:21:32 +0100

    Trabajando con ControladoDos.jsx

--- repaso-entrevista/src/componentes/componentControlados/ControladoDos.jsx
+++ repaso-entrevista/src/componentes/componentControlados/ControladoDos.jsx
@@ -1,1 +1,19 @@
-expo;
+export default function ControladoDos() {
+  return (
+    <form>
+      <label>
+        Nombre
+        <input type="text" />
+      </label>
+      <label>
+        Email
+        <input type="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" />
+      </label>
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}

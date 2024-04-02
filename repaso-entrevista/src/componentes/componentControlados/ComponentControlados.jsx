c31098b830f934659d5484a855d8d687f53399e2
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-02 12:43:02 +0100

    Trabajando en el formulario controlado

--- repaso-entrevista/src/componentes/componentControlados/ComponentControlados.jsx
+++ repaso-entrevista/src/componentes/componentControlados/ComponentControlados.jsx
@@ -1,18 +1,50 @@
 import "./ComponentControlados.css";
+import { useState } from "react";
 export default function ComponentControlados() {
+  const [values, setValues] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(name, value);
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(values);
+  };
   return (
-    <form>
-      <label>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nombre">
         Nombre
-        <input type="text" />
+        <input
+          type="text"
+          name="nombre"
+          value={values.nombre}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Email
-        <input type="email" />
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Password
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
       </label>
       <button type="submit">Enviar</button>
     </form>

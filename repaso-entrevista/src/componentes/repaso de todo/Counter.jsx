ddcd9e279b83fbb851ec236b307e329cf48fcc32
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-07 15:51:16 +0100

    Trabajando en el hook useEffect en el componente Counter.jsx

--- repaso-entrevista/src/componentes/repaso de todo/Counter.jsx
+++ repaso-entrevista/src/componentes/repaso de todo/Counter.jsx
@@ -1,25 +1,8 @@
 import { useEffect, useState } from "react";
 import "./Counter.css";
 
-// Simulamos un servicio que nos da un numero aleatorio cada segundos
-const randomService = {
-  subscribe: (callback) => {
-    return setInterval(() => {
-      const randomNumber = Math.floor(Math.random() * 100);
-      callback(randomNumber);
-    }, 1000);
-  },
-  unsubscribe: (id) => {
-    clearInterval(id);
-  },
-};
-
 export default function Counter() {
   const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const handleNewRandomNumber = (newNumber) => {};
-  }, []);
 
   useEffect(() => {
     document.title = `Contador: ${counter}`;

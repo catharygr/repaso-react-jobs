4157532b95736993bdc72cc67a0a9857723a8d18
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-09 12:37:58 +0100

    Trabajando en el componente Clock.jsx

--- repaso-entrevista/src/componentes/repaso de todo/Clock.jsx
+++ repaso-entrevista/src/componentes/repaso de todo/Clock.jsx
@@ -2,13 +2,6 @@ import { useEffect, useState } from "react";
 export default function Clock() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="reloj">
       <h2>Reloj</h2>

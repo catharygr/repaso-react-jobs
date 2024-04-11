45806b66e1ad216f56c48fbd651bee4e009c07d7
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-11 11:04:45 +0100

    Trabajando con fetch en Publicaciones.jsx

---
+++ repaso-entrevista/src/componentes/publicaciones/Publicaciones.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Publicaciones</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Posts;

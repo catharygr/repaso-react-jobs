dabb821a62d8fda7181658bd8957c98fc49ae5d9
catharygr <103951447+catharygr@users.noreply.github.com>
2024-04-18 13:04:45 +0100

    Trabajando con componentes creados con clss

--- repaso-entrevista/src/componentes/resumen/Resumen.jsx
+++ repaso-entrevista/src/componentes/resumen/Resumen.jsx
@@ -1,8 +1,29 @@
+import React from "react";
+
 export default function Resumen() {
   return (
     <div>
       <h1>Resumen</h1>
       <p>Resumiendo lo estudiado</p>
+      <Counter />
     </div>
   );
+}
+
+export class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Contador: {this.state.count}</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Incrementar
+        </button>
+      </div>
+    );
+  }
 }

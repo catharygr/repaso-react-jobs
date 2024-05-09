81c647f30ee5ed3d0afa5b99a1f6a6a01b82307b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-09 15:12:02 +0100

    Trabajando en el formulario

---
+++ repaso-entrevista/src/componentes/minimalista/FormularioMinimalista.jsx
@@ -0,0 +1,32 @@
+export default function FormularioMinimalista() {
+  return (
+    <form
+      style={{ display: "flex", flexDirection: "column", maxWidth: "300px" }}
+    >
+      <label htmlFor="nombre">
+        <VisuallyHidden>Nombre</VisuallyHidden>
+        <input
+          id="nombre"
+          type="text"
+          placeholder="Nombre"
+          style={{ margin: "10px 0", padding: "10px" }}
+        />
+      </label>
+      <label htmlFor="email">
+        <VisuallyHidden>Email</VisuallyHidden>
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          style={{ margin: "10px 0", padding: "10px" }}
+        />
+      </label>
+      <button
+        type="submit"
+        style={{ padding: "10px", marginTop: "10px" }}
+      >
+        Enviar
+      </button>
+    </form>
+  );
+}

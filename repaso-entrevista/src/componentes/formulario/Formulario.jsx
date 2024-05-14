6d7a42f8120161751b2837af859445735206fb16
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-14 13:26:19 +0100

    Trabajando en el componente Formulario.jsx

--- repaso-entrevista/src/componentes/formulario/Formulario.jsx
+++ repaso-entrevista/src/componentes/formulario/Formulario.jsx
@@ -88,6 +88,14 @@ export default function Formulario() {
       </div>
       <div>
         <p className={theme}>{theme}</p>
+        <button
+          className="btn-theme"
+          onClick={() =>
+            setTheme(theme === "modo-claro" ? "modo-oscuro" : "modo-claro")
+          }
+        >
+          Cambiar tema
+        </button>
       </div>
       <div>
         <p>{miConstante.current}</p>

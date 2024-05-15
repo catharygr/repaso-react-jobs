c2139f2b5bb1ecabc017d22a4e74ddb7bd33fade
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-15 13:47:24 +0100

    Trabajando en el componente Formulario.jsx

--- repaso-entrevista/src/componentes/formulario/Formulario.jsx
+++ repaso-entrevista/src/componentes/formulario/Formulario.jsx
@@ -39,53 +39,52 @@ export default function Formulario() {
 
   return (
     <>
-      <div className="container">
-        <form onSubmit={manejarSubmit}>
-          <label
-            ref={miBoton}
-            htmlFor="nombre"
-          >
-            Nombre
-          </label>
-          <input
-            className="input-formulario"
-            type="text"
-            value={form.nombre}
-            onChange={(e) => setForm({ ...form, nombre: e.target.value })}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            className="input-formulario"
-            type="email"
-            ref={inputRef}
-            value={form.email}
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
-          />
-          <button
-            className="btn"
-            type="submit"
-          >
-            Enviar
-          </button>
-        </form>
-        {modal && (
-          <div className="modal">
-            <p>Formulario enviado</p>
-            <button
-              className="btn-modal"
-              onClick={() => setModal(false)}
-            >
-              Cerrar
-            </button>
-          </div>
-        )}
-        <button
-          className="btn-focus"
-          onClick={handleClickFocus}
+      <form onSubmit={manejarSubmit}>
+        <label
+          ref={miBoton}
+          htmlFor="nombre"
         >
-          Focus
+          Nombre
+        </label>
+        <input
+          className="input-formulario"
+          type="text"
+          value={form.nombre}
+          onChange={(e) => setForm({ ...form, nombre: e.target.value })}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          className="input-formulario"
+          type="email"
+          ref={inputRef}
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <button
+          className="btn"
+          type="submit"
+        >
+          Enviar
         </button>
-      </div>
+      </form>
+      {modal && (
+        <div className="modal">
+          <p>Formulario enviado</p>
+          <button
+            className="btn-modal"
+            onClick={() => setModal(false)}
+          >
+            Cerrar
+          </button>
+        </div>
+      )}
+      <button
+        className="btn-focus"
+        onClick={handleClickFocus}
+      >
+        Focus
+      </button>
+
       <div>
         <p className={theme}>{theme}</p>
         <button

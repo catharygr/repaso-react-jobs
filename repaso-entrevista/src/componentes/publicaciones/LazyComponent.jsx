5fdeb938b6c6625f8bf2c7094591ad63bbfaeecd
catharygr <103951447+catharygr@users.noreply.github.com>
2024-05-18 12:49:46 +0100

    Importanción del componente Suspense

--- repaso-entrevista/src/componentes/publicaciones/LazyComponent.jsx
+++ repaso-entrevista/src/componentes/publicaciones/LazyComponent.jsx
@@ -1,14 +1,7 @@
-import React, { Suspense } from "react";
-
-const LazyComponent = React.lazy(() => import("./LazyComponent"));
-
 export default function MyComponent() {
   return (
     <div>
       <h2>Lazy Component</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyComponent />
-      </Suspense>
     </div>
   );
 }
